181f779c022509643f2e0dfe1d18d4d592e3455f
carla235 <cs235@hdm-stuttgart.de>
2017-11-08 10:53:16 +0100

    Code Änderung

--- index.php
+++ index.php
@@ -6,10 +6,89 @@
  * Time: 15:44
  */
 
-echo "test";
-echo "test2";
-echo "test3";
-echo "test4";
-echo "test5";
-echo "test6";
-echo "test7";
+
+
+session_start();
+
+echo "
+
+<!DOCTYPE html>
+<html lang=\"en\">
+<head>
+    <meta charset=\"UTF-8\">
+    <title>ANNE KERN Concept Store</title>
+    
+</head>
+<body>
+
+
+    <table> 
+         <td><a href='#'><img src='Logo.png' width='50px' height='auto'></a></td>
+    </table>
+    <br> 
+
+
+
+
+
+  
+           if (isset($_SESSION['userid'])) {
+               echo"   <li><a href=\"index.php?page=logout\">Ausloggen</a></li>"; }
+           else{
+               echo"  <li><a href=\"index.php?page=login\">Einloggen</a></li>";}
+echo"
+            <li><a href=\"#\">About Me</a></li>
+            <li><a href=\"#\">Andere Aktion</a></li>
+            
+          </ul>
+        </li>
+      </ul>
+      
+     
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>";
+
+
+if (isset($_GET["page"]) ) {
+    switch ($_GET["page"]) {
+        case "blogposts":
+            include "system/blogposts/posts.php";
+            break;
+
+        case "login":
+            include "system/login/login.php";
+            break;
+
+        case "logout":
+            include "system/login/logout.php";
+            break;
+
+        case "register":
+            include "system/login/register.php";
+            break;
+
+        case "edit":
+            //?id=".$_GET["id"]
+            include "system/blogposts/bearbeiten.php";
+            break;
+
+        case "löschen":
+            include "system/blogposts/löschen.php";
+            break;
+
+        default:
+            include "system/start.php";
+            break;
+
+
+    }
+}
+else
+{
+    include "system/start.php";
+}
+echo"<br></body>
+    </html>";
