0e6cffe6e44eeb56f93a57bb02c561ea808a94a7
annaosterloh <ao027@hdm-stuttgart.de>
2017-11-16 14:26:54 +0100

    code

--- system/start.php
+++ system/start.php
@@ -5,20 +5,25 @@
  * Date: 13.11.17
  * Time: 10:17
  */
-echo "<html>
+echo
+"<html>
 <head>
-<title></title>
+    <title>START</title>
 </head>
+
 <body>
+
 <div align='center'><a href='#'><img src='./titelbild.jpg' width='500px' height='250px'></a></div>
+
 <p>
 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
 </p>
 
-<div align='left'><a href='#'><img src='./klamotten.jpg' width='100px' height='100px'></a></div>
-<div align='center'><a href='#'><img src='./deko.jpg' width='100px' height='100px'></a></div>
-<div align='right'><a href='#'><img src='./laden.jpg' width='100px' height='100px'></a></div>
-
+<p>
+<div align='left'><a href='#'><img src='klamotten.jpg' width='100px' height='100px'></a></div>
+<div align='center'><a href='#'><img src='deko.jpg' width='100px' height='100px'></a></div>
+<div align='right'><a href='#'><img src='laden.jpg' width='100px' height='100px'></a></div>
+<p>
 </body>
 
 </html>
