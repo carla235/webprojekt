5b1bf17afb7d03840547c487e945d61dd109fe0e
carla235 <cs235@hdm-stuttgart.de>
2017-11-21 13:25:58 +0100

    Code Änderung

--- system/account/userdata.php
+++ system/account/userdata.php
@@ -25,5 +25,6 @@ $dsn    = "mysql:dbhost=localhost;dbname=u-cs235";
 $dbuser = "cs235";
 $dbpass = "ohBahphae2";
 $option = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
-
+/* hier lege ich die jeweiligern Variablen fest*/
 ?>
+
--- system/shop.php
+++ system/shop.php
@@ -11,15 +11,15 @@ include_once(dirname(__FILE__)."/./account/userdata.php");
 
 try {
     $db = new PDO($dsn, $dbuser, $dbpass, $option);
-    $sql = "SELECT * FROM produktkatalog ORDER BY date DESC ";
+    $sql = "SELECT * FROM produktkatalog";
     $query = $db->prepare($sql);
     $query->execute();
 
     while ($zeile = $query->fetchObject()) {
         echo "<div class='produkte'>";
         echo "<br>";
-        echo "<div class='artikelname'>$zeile->artikelname - 
-            Marke: $zeile->marke";
+        echo "<div class='artikelname'>$zeile->marke - 
+            $zeile->artikelname";
         echo "</div><br>";
         if (!empty($zeile->bild)) {
             echo "<p><img src='./system/produkte/$zeile->images'></p><br>";
@@ -28,4 +28,14 @@ try {
         echo $zeile->preis . "<br>";
         echo "</div>";
     }
-    echo "</div>";}
+echo "</div><br>";
+
+
+
+
+     $db = null;
+} catch (PDOException $e) {
+    echo "Error!: Bitte wenden Sie sich an den Administrator!?...".$e;
+    die();
+}
+?>
