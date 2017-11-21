79f1d763c4823045ce2c1fe4f12ecf5b241d9b48
annaosterloh <ao027@hdm-stuttgart.de>
2017-11-21 23:34:57 +0100

    code

--- system/produkte/Produktansicht.php
+++ system/produkte/Produktansicht.php
@@ -1,35 +1,31 @@
 <?php
-echo "<html>";
-echo "<br>";
-include(dirname(__FILE__)."/./account/userdata.php");
 
+include(dirname(__FILE__) . "/../account/userdata.php");
+echo"<html><body>";
 try {
     $db = new PDO($dsn, $dbuser, $dbpass, $option);
-    $sql = "SELECT * FROM produkt WHERE id=(SELECT MAX(id) FROM blogposts) ";
+    $sql = "SELECT * FROM produktkatalog  ";
     $query = $db->prepare($sql);
     $query->execute();
 
-    $zeile =$query->fetchObject();
-    if ($zeile == NULL) {
-        echo "Bla";
-    } else {
-        echo "<img scr='./system/images/$zeile->filename' width='200em' height='auto'> ";
-        echo "<p>".$zeile->content."</p><br>";
-        echo "  <p><img src='./system/blogposts/uploads/$zeile->filename' width='200em' height='auto' ></p>";
-    }
-    $db=null;}
+    while ($zeile = $query->fetchObject()) {
+        echo    "<img scr= './system/produkte/images/$zeile->bild' width='200em' height='auto' >
+                 <h1> $zeile->artikelname | $zeile->marke </h1><br>
+                 EAN:$zeile->ean <br>
+                 Preis: $zeile->preis <br>
+                 Artikelbeschreibung:<br>
+                 $zeile->artikelbeschreibung<br>
+                 Details:<br>
+                 $zeile->details<br>
+                 
+      
+      ";}
+      $db=null;}
 
 catch (PDOException $e) {
     echo "Error!: Bitte wenden Sie sich an den Administrator!?..." . $e;
-    die();
-}
-//echo "</div>";
-echo"<html>";
-?>
+    die();}
 
-/**
- * Created by PhpStorm.
- * User: anna-mariaosterloh
- * Date: 21.11.17
- * Time: 13:53
- */
+        echo "</body></html>";
+
+
