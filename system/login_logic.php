860c75214d961bdaffabc4b6adb54e59b2f74ef4
annaosterloh <ao027@hdm-stuttgart.de>
2017-11-28 12:23:06 +0100

    code

--- system/login_logic.php
+++ system/login_logic.php
@@ -1,47 +1,58 @@
 <?php
+session_start();
 include(dirname(__FILE__) . "/account/userdata.php"); // Inkludiere userdata
 
-session_start();
-$db = new PDO($dsn, $dbuser, $dbpass, $option);
+
+//$db = new PDO($dsn, $dbuser, $dbpass, $option);
 
 $email = htmlspecialchars($_POST["email"], ENT_QUOTES, "UTF-8");
 
-if (!empty($email)) { // Wenn email nicht leer wird Passwort geprüft
+
+if(!empty($email)) { // Wenn email nicht leer wird Passwort geholt aus Formular
     $errorMessage = "";
     $passwort = htmlspecialchars($_POST["passwort"], ENT_QUOTES, "UTF-8");
 
     $db = new PDO($dsn, $dbuser, $dbpass, $option);
-    $sql = "SELECT * FROM benutzer WHERE email= :email";
+    $sql = "SELECT * FROM benutzer WHERE email= :email  ";
     $query = $db->prepare($sql);
     $query->execute(array(':email' => $email));
 
     $zeile = $query->fetch();
 
 
+
+
     //Überprüfung des Passworts wenn e-mail stimmt
 
 
-    if($zeile !== false && password_verify($passwort, $zeile["passwort"])) {
-        $_SESSION['userid'] = $zeile['id'];
-       header:("Location:index.php");
+   if($zeile !==false && password_verify($passwort, $zeile["passwort"])) {
+        $_SESSION['kundennummer'] = $zeile['kundennummer'];
+       header:("Location:'../index.php'");
        die();
+
+
     }
+
+
+
     else {
         $errorMessage = "E-Mail oder Passwort war ungültig<br>";
     }
-} else $errorMessage = "";
+} else $errorMessage = "Fehler";
 
 
-if (!empty($errorMessage)) {
+
+/*if (!empty($errorMessage)) {
 
     echo "<html> 
     <head><meta charset=\"UTF-8\">
     
-    <body>" . $errorMessage;
+    <body>" ;
+    include 'index.php';
     echo "
     </body>
     </html>";
-}
+}*/
 
 /**
  * Created by PhpStorm.
