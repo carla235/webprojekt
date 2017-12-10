c1e909d9e63c0e348a7b7950ff261f8464ec4422
mMarID <m.idkhafif@gmail.com>
2017-12-10 22:14:25 +0100

    Logout - Benachrichtigung

--- system/logout.php
+++ system/logout.php
@@ -6,4 +6,31 @@
  * Time: 12:30 PM
  */
 session_destroy();
-echo "Abgemeldet!";
+
+echo "Sie wurden erfolgreich Abgemeldet.";
+
+echo " 
+ <html> 
+ <head> 
+ <style> 
+ div {
+    background-color: white;
+    color: black ;
+    padding: 2px 5px;
+    margin: 8px 0;
+    cursor: pointer;
+    width: 175px; 
+    border: 1px solid black;
+} 
+
+ 
+ </style>
+ </head>
+ <body> <div> <a href='index.php?page=start'>Zurück zur Startseite </a></li> </div>
+ </body>
+ </html> 
+ ";
+
+
+
+?>
