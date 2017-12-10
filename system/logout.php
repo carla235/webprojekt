6211a7da56915abe9a52f4ac62cd71f75e261d63
mMarID <m.idkhafif@gmail.com>
2017-12-10 23:03:21 +0100

    Logout - IncludeBenachrichtigung

--- system/logout.php
+++ system/logout.php
@@ -7,30 +7,7 @@
  */
 session_destroy();
 
-echo "Sie wurden erfolgreich Abgemeldet.";
-
-echo " 
- <html> 
- <head> 
- <style> 
- div {
-    background-color: white;
-    color: black ;
-    padding: 2px 5px;
-    margin: 8px 0;
-    cursor: pointer;
-    width: 175px; 
-    border: 1px solid black;
-} 
-
- 
- </style>
- </head>
- <body> <div> <a href='index.php?page=start'>Zurück zur Startseite </a></li> </div>
- </body>
- </html> 
- ";
-
-
+include "../system/logout_confirmation.php";
 
 ?>
+
