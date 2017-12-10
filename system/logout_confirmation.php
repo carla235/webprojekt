887876d0e302a1b7c105d66b692009eaa8d24281
mMarID <m.idkhafif@gmail.com>
2017-12-10 23:08:09 +0100

    Logout - Benachrichtigung

---
+++ system/logout_confirmation.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zigza
+ * Date: 12/10/2017
+ * Time: 10:20 PM
+ */
+
+echo "Sie wurden erfolgreich Abgemeldet.";
+
+echo " 
+ <html> 
+ <head> 
+ <style> 
+ div {
+    background-color: white;
+    color: black ;
+    padding: 2px 5px;
+    margin: 8px 0;
+    cursor: pointer;
+    width: 175px; 
+    border: 1px solid black;
+} 
+
+ 
+ </style>
+ </head>
+ <body> <div> <a href='../index.php?page=start'>Zurück zur Startseite </a></li> </div>
+ </body>
+ </html> 
+ ";
