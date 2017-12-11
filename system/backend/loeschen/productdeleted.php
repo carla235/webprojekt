670c57c801109833838ba1b66809534f1cd7b9c6
mMarID <m.idkhafif@gmail.com>
2017-12-11 04:59:19 +0100

    Benachrichtigung, dass Produkt geloescht wurde

---
+++ system/backend/loeschen/productdeleted.php
@@ -0,0 +1,26 @@
+<?php
+
+echo "Das Produkt wurde erfolgreich gelöscht.";
+
+echo " 
+ <html> 
+ <head> 
+ <style> 
+ div {
+    background-color: white;
+    color: black ;
+    padding: 2px 5px;
+    margin: 8px 0;
+    cursor: pointer;
+    width: 175px; 
+    border: 1px solid black;
+} 
+
+ 
+ </style>
+ </head>
+ <body> <div> <a href='../../../index.php?page=shop'>Zurück zum Shop</a> </div>
+ </body>
+ </html> 
+ ";
+//<a href='../shop.php?page=shop'>Zurück zum Shop</a>
