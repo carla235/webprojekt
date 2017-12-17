86d9273da579aee013cadb0f0086ebc5895d72dd
annaosterloh <ao027@hdm-stuttgart.de>
2017-12-17 20:27:06 +0100

    Warenkorb funktioniert

--- system/bestätigung.php
+++ system/bestätigung.php
@@ -1,6 +1,30 @@
 <?php
 
+echo"
+<html> 
+<head>
+<title>Vielen Dank!</title>
+<style>
+h1{
+font-family: Arial;
+font-weight: lighter;
+}
+</style>
+</head>
+<body>
+<div align='center'>
+<img src='../herz.png' height='200px' width='auto'>
+<h1>Vielen Dank für deine Bestellung! </h1>
+</div>
 
+</body>
+
+
+
+</html>
+
+
+";
 /**
  * Created by PhpStorm.
  * User: anna-mariaosterloh
