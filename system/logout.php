effa8b9197c426523b20ae1e8f0dbae4a942f050
mMarID <m.idkhafif@gmail.com>
2018-01-08 04:21:15 +0100

    logout

--- system/logout.php
+++ system/logout.php
@@ -4,12 +4,7 @@
  * User: zigza
  * Date: 11/20/2017
  * Time: 12:30 PM
- if (isset($_SESSION['kundennummer'])) { // Prüfen, ob Session existiert
 
-
-        echo "<a href= '/system/logout_confirmation.php'><img src='../logout.png' height='20px' width='auto'></a></div>";
-
-}
 */
 include"logout_confirmation.php";
 ?>
