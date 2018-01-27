437a57e2e422638a1eb1df8e0a1fba2a28886baa
carla235 <cs235@hdm-stuttgart.de>
2018-01-27 19:29:04 +0100

    Code Änderung: Footer Info

---
+++ system/footer info/datenschutz.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: carlastumpp
+ * Date: 27.01.18
+ * Time: 19:26
+ */
+echo
+"<html>
+<head>
+
+    <title>datenschutz</title>
+    <style>
+
+    .text{
+
+  width: 900px;
+  }
+
+</style>
+</head>
+
+<body>
+
+Datenschutz<br />
+
+<br />
+<p style=\"font-weight: bold; padding-bottom: 10px;\">Quellen von Icons!!!</p>
+
+    </body>
+  ";
