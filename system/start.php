55610a6af0c1f5e0024c12d1e830508bd0e35111
carla235 <cs235@hdm-stuttgart.de>
2018-01-29 14:03:29 +0100

    Code Änderung: Footer Info

--- system/start.php
+++ system/start.php
@@ -34,10 +34,11 @@ echo
 <div class='col-sm-12'>
 
       <img class=\"d-block w-100\" src=\"./images/startak.jpg\" >
+      </div>
   
    
 
-
+<div class='col-sm-4 umrandung_1'>
 <p class=\"text-left\">
 Wir möchten Menschen in toller Atmosphäre überraschen, sie inspirieren und immer wieder auf’s Neue begeistern. Im Concept Store gibt es individuelle Damenmode, auch stylische, nachhaltige Mode „made fair“. Zum besonderen Produkt- und Markenangebot spielt das Gespräch untereinander und das Erlebnis beim Einkauf eine große Rolle. Hier geht es um Wohlfühlatmosphäre, erstklassigen Kundenservice und eben dieses besondere Markenangebot. Das moderne Erlebniskonzept verbindet Mode, Kunst und Design. Vor allem aber soll der Concept Store ein Ort sein, an dem sich die Kunden entspannen können und sich gerne aufhalten.
 </p>
