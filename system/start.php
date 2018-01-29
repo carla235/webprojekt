558e16291a27e1b0542d63b9f40ee2d98cf40f5c
carla235 <cs235@hdm-stuttgart.de>
2018-01-29 14:55:57 +0100

    Code Änderung: Grid Anordnung Home

--- system/start.php
+++ system/start.php
@@ -58,15 +58,16 @@ echo
 Wir möchten Menschen in toller Atmosphäre überraschen, sie inspirieren und immer wieder auf’s Neue begeistern. Im Concept Store gibt es individuelle Damenmode, auch stylische, nachhaltige Mode „made fair“. Zum besonderen Produkt- und Markenangebot spielt das Gespräch untereinander und das Erlebnis beim Einkauf eine große Rolle. 
 </p>
 </div>
+<div class=\"col-sm\">
+<img src='./images/Blumenkranz_Prozente_besser.jpg'  width='100%' height='auto'>
+</div><br>
 
 <div class='col-sm-4 umrandung_2'>
 <p class=\"text-left\">
 Hier geht es um Wohlfühlatmosphäre, erstklassigen Kundenservice und eben dieses besondere Markenangebot. Das moderne Erlebniskonzept verbindet Mode, Kunst und Design. Vor allem aber soll der Concept Store ein Ort sein, an dem sich die Kunden entspannen können und sich gerne aufhalten.
 </p>
 </div>
-<div class=\"col-sm\">
-<img src='./images/Blumenkranz_Prozente_besser.jpg'  width='100%' height='auto'>
-</div><br>
+
 
 
 <div class='col-sm-12'>
