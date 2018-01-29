8185f79f1f2505441f30f8a85939df931f37aaf8
annaosterloh <ao027@hdm-stuttgart.de>
2018-01-29 15:51:54 +0100

    CSS

--- index.php
+++ index.php
@@ -31,9 +31,12 @@ echo "
 
 <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js\" integrity=\"sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl\" crossorigin=\"anonymous\"></script>
 
+<link href=\"https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300\" rel=\"stylesheet\">
     <style>
+    
 
 body {
+font-family: 'Open Sans Condensed', sans-serif;
 margin-left: 10%;
 margin-right: 10%;
 }
