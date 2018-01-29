1b7bdb53c50f105d4e2469b6a1288b38dfe0d79d
carla235 <cs235@hdm-stuttgart.de>
2018-01-29 17:30:38 +0100

    Code Änderung: Grid Anordnung Home

--- system/contact.php
+++ system/contact.php
@@ -24,9 +24,7 @@ echo "<html>
     }
     
       .contact{
-      position: relative;
-      float: left;
-      left: 60%;
+      left: 30%;
       margin-right: 10%;
       margin-top: -20%;  
       }
@@ -48,27 +46,82 @@ echo "<html>
       padding: auto; 
       }
       
+      .umrandung_1{
+ border-style: dotted;
+  border-width: 1.5px;
+  margin-left: 1.1%;
+  margin-right:5px;
+  margin-top: 5%;
+  margin-bottom: 5%;
+  padding: 1%;
+  padding-left:5%;
+  padding-right:5%;
+  padding-bottom:5%;
+  padding-top:7%;
+  }
+  
+  .inhaberin{
+margin-left: 0.1%;
+  margin-right:1px;
+  margin-top: 5%;
+  margin-bottom: 5%;
+
+  }
+      
+ .map{
+margin-left: 0.1%;
+  margin-right:1px;
+  margin-top: 11%;
+  margin-bottom: 5%;
+
+  }
+      
     </style>
 
 
 </head>
 <body>
-<h1 id='contactus'>CONTACT US!</h1> <br>
-<a href='https://www.google.de/maps/place/Anne+Kern+Concept+Store/@48.4767309,8.9346626,15z/data=!4m5!3m4!1s0x0:0xdc434e420bca24da!8m2!3d48.4767309!4d8.9346626'><img src='./Map.png' width='50%' height='auto%'></a>
-<br>
 
-<div class='contact'>
-Marktstraße 11 <br>
-Rottenburg<br>
-Baden-Wurttemberg, Germany<br> <br>
-<h1>FOLLOW US!</h1> <br> 
+<div class=\"container\">
+<div class='col-sm-12>
+<h1 id='contactus'>CONTACT US!</h1> <br>
+</div>
+  <div class=\"row\">
+  <div class=\"col-sm-6 inhaberin\">
+<img src='./images/inhaberin.jpg'  width='80%' height='auto'>
+</div><br>
+<div class='col-sm-4 umrandung_1'>
+<p class=\"text-left\">
+„Ich möchte Menschen mit meinem Produkt- und Markenangebot in toller Atmosphäre regelmäßig überraschen, sie inspirieren und immer wieder aufs Neue begeistern“
+<br><br>
+<h2>FOLLOW US!</h2> <br> 
 
     <div class='Social'>
         <a href='https://www.instagram.com/annekern.conceptstore/?hl=de'><img src='./instagram.png'width='50px' height='50px'></a>
         <a href='https://www.facebook.com/annekern.conceptstore/'><img src='./facebook.png' width='50px' width='50px'></a>
         <div id='kontaktaufnahme'><a href='./system/kontaktaufnahme.php'> ANNE KERN Concept Store kontaktieren </a></div>
     </div>
-</div> 
+</p>
+</div>
+
+<div class=\"container\">
+
+<div class=\"row\">
+  <div class=\"col-sm-6 map\">
+<a href='https://www.google.de/maps/place/Anne+Kern+Concept+Store/@48.4767309,8.9346626,15z/data=!4m5!3m4!1s0x0:0xdc434e420bca24da!8m2!3d48.4767309!4d8.9346626'><img src='./Map.png' width='80%' height='auto'></a>
+</div><br>
+<div class='col-sm-4 umrandung_1'>
+
+<p class=\"text-left\">
+Marktstraße 11 <br>
+72108 Rottenburg<br>
+Baden-Wurttemberg, Germany<br>
+Tel:  07472/4405268 <br>
+Mo-Sa, 10:00-18:30 <br><br>
+ 
+</p>
+</div>
+
 
 
 </body>
