07481fe7a55de592643f8a53db0ae539eacdb4cc
mMarID <m.idkhafif@gmail.com>
2018-02-04 22:26:40 +0100

    summe-Err

--- system/bestellvorgang/warenkorb.php
+++ system/bestellvorgang/warenkorb.php
@@ -44,17 +44,21 @@ top: -150px;
 
         if (isset($_SESSION['warenkorb'])) { // Prüfen, ob Session-Variable für den Warenkorb existiert
             foreach($_SESSION['warenkorb']as $neu){ // Gibt Artikelinformationen aus Session array aus
-                $id=$neu['artikelnummer'];
+                $id= $neu['artikelnummer'];
                 $a = $neu['bild'];
                 echo "<a href='#'><img src= './images/$a' width='100px' height='auto'></a>";
 
                 echo "<div class='produktinfos'>";
-                echo $neu['artikelname']." |  ";
+                echo $neu ['artikelname']." |  ";
                 echo $neu['marke']."<br>";
                 echo $neu['groesse']." | ";
                 echo $neu['menge']."<br>";
 
                 echo $neu['preis']."€<br><br><br>";
+
+                echo "<input type='text' name='menge' placeholder='Menge' class='form_control' value=''>"; echo "<br>";echo "<br>";
+                echo "<input type='hidden' name='hidden_preis' value='<?php echo $row\['preis']; >";
+
                 echo "<a href= './system/bestellvorgang/delete_wk.php?delete=$id'><img src='./cross.png' height='20px' width='auto'></a></div>";
 
 
@@ -68,21 +72,39 @@ echo "
 <h3>SUMME DEINER BESTELLUNG!</h3>";
 
 if (isset($_SESSION['warenkorb'])) { // Prüfen, ob Session-Variable für den Warenkorb existiert
-    foreach($_SESSION['warenkorb']as $neu){ // Gibt artikelname und Preis aus Session array aus
-
+    foreach($_SESSION['warenkorb'] as $neu){ // Gibt artikelname und Preis aus Session array aus
 
     echo $neu['artikelname']." | ";
-    echo $neu['preis']."€<br><br><br>";
+    echo $neu['preis']."€<br><br>";
 
 
     }
     }
-    echo"Versand: 4,90€ <br><br><br>";
+/*if (!empty($_SESSION['warenkorb'])){
+     $total = 0;
+     foreach($_SESSION['warenkorb']as $keys => $values) {
+         //echo $values['artikelname'];
+         //echo $values['menge'];
+         //echo $values['preis'];
+         echo number_format( $values['preis']+$values['preis'], 2); echo "<br>";
+         //$values['menge']*
+     }
+}
+*/
+
+if(isset($_SESSION['warenkorb'])) {
+     foreach($_SESSION['warenkorb'] as $neu){
+
+        $summe = $neu['preis'];
+}
+}
+    //$summe = number_format($row['preis']+$row['preis'],3);
+    echo "Versand: 4.90€"; echo "<br>";echo "<br>";
 
 
 
 
-    $p=   + 4.90 ;
+    $p=  $summe + 4.90 ;
     echo"Summe: $p €<br><br><br>
 <a href='index.php?page=kaufen'>
 <input type='submit' value='KAUFEN!' class='button'/></a>
